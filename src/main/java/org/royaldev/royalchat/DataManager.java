7e5d9e2317b5c96ac2e6b2f23ced9575e2e3fdcb
Kyle Clemens <jkc.clemens@gmail.com>
2014-06-17 03:24:02 -0400

    Fixed: Herp

--- src/main/java/org/royaldev/royalchat/DataManager.java
+++ src/main/java/org/royaldev/royalchat/DataManager.java
@@ -285,6 +285,7 @@ public class DataManager {
             message = message.replace("{#inchannel}", (isNull) ? "0" : String.valueOf(c.getMembers().size()));
         }
         if (sanitize) message = sanitizeChat(message);
+        return message;
     }
 
     /**
